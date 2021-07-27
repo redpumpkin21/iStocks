80bb942c3167ef8c201e462a9646b583a888bd37
Christopher Johnson <christopherjohnson@Christophers-MacBook-Pro.local>
2021-07-27 18:04:31 -0400

    finished

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const About = (props) => {
-    return <h1> STOCKS STOCKS STOCKS</h1>
+    return <h1> STOCKS STOCKS STOCKS, iStock delivers what you need. Stock prices and their symbol. Don't want to go to your stock page and be tempted to buy? We are here for you.</h1>
 }
 export default About
